b8ca7cc4b88ccb513e52abe76287ee36e106e373
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-28 11:08:19 +0100

    Added Teacher Devise model

---
+++ spec/features/teacher_features_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "Teacher signing up" do
+	context 'validates email' do
+	  it "can sign up with @makersacademy.com domain" do
+	  	teacher = build :teacher
+	  	visit '/teachers/sign_up'
+	    fill_in "Email", with: teacher.email
+	    fill_in "Password", with: teacher.password
+	    fill_in "Password confirmation", with: teacher.password_confirmation
+	    click_button "Sign up"
+	    expect(page).to have_content "Welcome! You have signed up successfully."
+	  end
+
+	  it "cannot sign up with any other domain" do
+	  	teacher = build :not_teacher
+	  	visit '/teachers/sign_up'
+	    fill_in "Email", with: teacher.email
+	    fill_in "Password", with: teacher.password
+	    fill_in "Password confirmation", with: teacher.password_confirmation
+	    click_button "Sign up"
+	    expect(page).to have_content "Sign up 1 error prohibited this teacher from being saved: Email is invalid"
+	  end
+	end
+end
