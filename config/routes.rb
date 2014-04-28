8460e4b943524d69e09ea8ad8beed5bc1264586b
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-28 15:20:42 +0100

    Added Cohorts dropdown to teachers dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :teachers, only:[:index]
-  resources :cohorts, only:[:new, :create]
+  resources :cohorts, only:[:new, :create, :edit, :update]
 
   # You can have the root of your site routed with "root"
   root 'pages#index'
