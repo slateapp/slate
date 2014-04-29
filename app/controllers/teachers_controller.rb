6f3997a2b70b8eedf0ae850840e9bed5f0da6de6
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-29 18:26:27 +0100

    Switched storing cohort from name to id

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,12 +2,13 @@ class TeachersController < ApplicationController
   before_action :authenticate_teacher!, only: [:dashboard]
   def dashboard
     @teacher = current_teacher
+    @default = Cohort.find(@teacher.cohort) if @teacher.cohort
   end
 
   def update
     @teacher = current_teacher
     @cohort = Cohort.find params[:cohort][:id]
-    @teacher.cohort = @cohort.name
+    @teacher.cohort = @cohort
     @teacher.save
     redirect_to dashboard_teachers_path
   end
