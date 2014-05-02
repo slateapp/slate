879c2ddcdeb87185804a53027cf8db24730323ee
Ross Hepburn <hepburnjrl@gmail.com>
2014-05-02 10:42:48 +0100

    Fixed GitHub Omniauth for localhost

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	if ENV['development'] || ENV['test']
+	if ENV['production']
+		provider :github, ENV['_SECRET_GITHUB_KEY'], ENV['_SECRET_GITHUB_SECRET'], scope: 'user' 
+	else
    provider :github, Rails.application.secrets.GITHUB_KEY, Rails.application.secrets.GITHUB_SECRET, scope: 'user'
- else
-   provider :github, ENV['_SECRET_GITHUB_KEY'], ENV['_SECRET_GITHUB_SECRET'], scope: 'user'
  end
-
 end
