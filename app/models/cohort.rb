bc32844bad6f57478ad7b94a202ba28be9ab7a69
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-02 16:33:48 +0100

    Dropdowns for creating a cohort

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,5 +1,11 @@
 class Cohort < ActiveRecord::Base
 	has_many :students
+  validates :month, :year, :presence => true
+  
+  def name
+    self.month + " " + self.year
+  end
+
   def name_to_date
     Date.parse(self.name)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501184619) do
+ActiveRecord::Schema.define(version: 20140502151851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,9 +33,10 @@ ActiveRecord::Schema.define(version: 20140501184619) do
   end
 
   create_table "cohorts", force: true do |t|
-    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "month"
+    t.string   "year"
   end
 
   create_table "requests", force: true do |t|
