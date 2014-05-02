8668f618c7dfcb7fadc7ebc1fb878d449e92be90
Ross Hepburn <hepburnjrl@gmail.com>
2014-05-02 08:20:09 +0100

    Changed Variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+	if ENV['development'] || ENV['test']
    provider :github, Rails.application.secrets.GITHUB_KEY, Rails.application.secrets.GITHUB_SECRET, scope: 'user'
+ else
+   provider :github, ENV['_SECRET_GITHUB_KEY'], ENV['_SECRET_GITHUB_SECRET'], scope: 'user'
+ end
+
 end
