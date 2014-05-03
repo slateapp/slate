9701a8c93dc9f16fb32b1665b543c2f2c7e1f473
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-03 14:09:08 +0100

    Cleaned up specs

--- spec/features/cohorts/cohort_features_spec.rb
+++ spec/features/cohorts/cohort_features_spec.rb
@@ -1,46 +1,49 @@
 require "spec_helper"
 
-describe "Cohorts" do
-  context 'not logged in as a teacher' do
-    it 'returns an error' do
-      visit '/cohorts/new'
-      expect(page).to have_content 'You need to sign in or sign up before continuing'
-    end
+describe "Creating a cohort" do
+  specify "returns an error if not signed in as a teacher" do
+    visit new_cohort_path
+    expect(page).to have_content 'You need to sign in or sign up before continuing'
   end
-  context 'logged in as a teacher' do
-    before(:each) do
-      login_as create :teacher
-    end
 
-    it "can be created by a teacher" do
-      visit '/teachers/dashboard'
-      click_link "Cohorts"
-      click_link "Add Cohort"
-      select('January', :from => 'cohort_month')
-      select('2020', :from => 'cohort_year')
-      click_button 'Create Cohort'
-      expect(page).to have_content "January 2020"
-    end
+  specify "is successful if signed in as a teacher" do
+    login_as create :teacher
+    visit new_cohort_path
+    select('January', :from => 'cohort_month')
+    select('2020', :from => 'cohort_year')
+    click_button 'Create Cohort'
+    expect(page).to have_content "January 2020"
+  end
+end
 
-    context 'a cohort has been created' do
-      before(:each) do
-        create :january
-        visit '/teachers/dashboard'
-        click_link "Cohorts"
-      end
-      
-      it "can be edited" do
-        click_link "Edit"
-        select('February', :from => 'cohort_month')
-        select('2014', :from => 'cohort_year')
-        click_button "Update Cohort"
-        expect(page).to have_content "February 2014"
-      end
+describe "Editing a cohort" do
+  before{ create :january }
+  specify "returns an error if not signed in as a teacher" do
+    visit edit_cohort_path(Cohort.last.id)
+    expect(page).to have_content 'You need to sign in or sign up before continuing'
+  end
 
-      it "can be deleted" do
-        click_link "Delete"
-        expect(page).not_to have_content "January 2014"
-      end
-    end
+  specify "is successful if signed in as a teacher" do
+    login_as create :teacher
+    visit edit_cohort_path(Cohort.last.id)
+    select('February', :from => 'cohort_month')
+    select('2014', :from => 'cohort_year')
+    click_button "Update Cohort"
+    expect(page).to have_content "February 2014"
+  end
+end
+
+describe "Deleting a cohort" do
+  before{ create :january }
+  specify "returns an error if not signed in as a teacher" do
+    visit cohorts_path
+    expect(page).to have_content 'You need to sign in or sign up before continuing'
+  end
+
+  specify "is successful if signed in as a teacher" do
+    login_as create :teacher
+    visit cohorts_path
+    click_link "Delete"
+    expect(page).not_to have_content "January 2014"
   end
 end
