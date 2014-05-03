c1c6997caccaa9984c5b9adf182341e9fff7259d
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-03 15:49:12 +0100

    Cleaning up update action

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,21 +1,25 @@
 class TeachersController < ApplicationController
   before_action :authenticate_teacher!, only: [:dashboard, :update, :students,
     :approve_student, :unapprove_student, :delete_student, :edit_student]
+
   def dashboard
     @requests = Request.all
-    @teacher = current_teacher
-    @cohorts_list = Cohort.all.sort_by(&:name_to_date).reverse
-    cohort = params[:cohort] ? params[:cohort] : @teacher.cohort
-    @cohort = Cohort.find(cohort) if cohort
-    @students = Student.where(cohort: cohort, approved: true)    
+    @teacher, @cohorts, @cohort_options = current_teacher, cohorts_in_order, cohort_options
+
+    @cohort = Cohort.find(selected_cohort) if selected_cohort
+    @students = Student.where(cohort: selected_cohort, approved: true)    
   end
 
   def update
-    @teacher = current_teacher
-    @cohort = Cohort.find params[:cohort][:id]
-    @teacher.cohort = @cohort
-    @teacher.save
-    redirect_to dashboard_teachers_path
+    teacher = current_teacher
+    teacher.cohort = Cohort.find params[:cohort][:id]
+    if teacher.save
+      flash[:success] = "Default cohort updated successfully"
+      redirect_to cohorts_path
+    else
+      teacher.errors.full_messages.each{ |msg| flash[:error] = msg }
+      redirect_to dashboard_teachers_path
+    end
   end
 
   def students
@@ -55,5 +59,18 @@ class TeachersController < ApplicationController
 
   def edit_student
     @student = Student.find params[:id]
+  end
+
+  private
+  def selected_cohort
+    params[:cohort] || @teacher.cohort
+  end
+
+  def cohorts_in_order
+    Cohort.all.sort_by(&:name_to_date).reverse
+  end
+
+  def cohort_options
+    cohorts_in_order.map { |cohort| [cohort.name, cohort.id] }  
   end
 end
