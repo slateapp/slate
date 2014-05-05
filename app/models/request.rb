e7a7eb845f3f8a7efd21f304c02d1439187b5386
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-05 15:57:13 +0100

    Added average queue length and average wait time stats to teacher dashboard

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,12 +4,15 @@ end
 class Request < ActiveRecord::Base
 	belongs_to :student
   validates :category, :description, :presence => true
+  scope :todays_solved_requests, -> { where(solved: true, solved_at: Time.now.beginning_of_day..Time.now) }
+  
   def category_name
     Category.find(self.category.to_i).name
   end
 
   def solve!
   	self.solved = true
+    self.solved_at = Time.now
   	save
   end
 
@@ -20,5 +23,29 @@ class Request < ActiveRecord::Base
   	else
   		update_attributes(attributes)
   	end
+  end
+
+  def time_to_solve
+    solved_at - created_at
+  end
+
+  def self.todays_average_wait_time
+    return 0 if todays_solved_requests.count == 0
+    total_wait_time = self.todays_solved_requests.inject(0){|sum, request| sum + request.time_to_solve}
+    (total_wait_time/self.todays_solved_requests.count)/60
+  end
+
+  def self.todays_average_queue
+    queue_lengths = []
+    minute = Time.now.beginning_of_day
+    while minute < Time.now
+      requests = self.where(created_at: Time.now.beginning_of_day..minute)
+      request_array = []
+      requests.each{ |request| request_array << request.category if request.category && (!request.solved || request.solved_at > minute) }
+      queue_lengths << request_array.count unless request_array.empty?
+      minute += 60
+    end
+    return 0 if queue_lengths.count == 0
+    queue_lengths.inject{|sum,length| sum + length}/queue_lengths.count
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140502151851) do
+ActiveRecord::Schema.define(version: 20140505145545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140502151851) do
     t.string   "category"
     t.integer  "student_id"
     t.boolean  "solved",      default: false
+    t.datetime "solved_at"
   end
 
   add_index "requests", ["student_id"], name: "index_requests_on_student_id", using: :btree
