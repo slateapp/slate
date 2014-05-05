003a001362a583ee8c81e08ea14c44823c958f4c
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-05 12:07:53 +0100

    Canot create more than one cohort the same

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,7 +1,13 @@
 class Cohort < ActiveRecord::Base
 	has_many :students
-  validates :month, :year, :presence => true
-  
+  validates :month, :year, presence: true
+  validate :cohort_uniqueness
+
+  def cohort_uniqueness
+    existing_record = Cohort.where(month: month, year: year).count
+    errors.add(month + " " + year, "has already been created!") if existing_record > 0
+  end
+
   def name
     self.month + " " + self.year
   end
