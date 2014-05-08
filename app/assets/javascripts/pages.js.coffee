fe2215fcbeab6d94fb7b1d5b97c2a3622f998509
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-08 07:14:39 +0100

    Approve or Unapprove all causes websocket trigger

--- app/assets/javascripts/pages.js.coffee
+++ app/assets/javascripts/pages.js.coffee
@@ -15,3 +15,18 @@ $(document).ready ->
 					console.log("Unapproved")
 					window.location.pathname = "/students/require_approval"
 		)
+
+	channel_batch_approval = dispatcher.subscribe 'student_batch_approval'
+	channel_batch_approval.bind 'student_batch_approval', (students) ->
+		$.each(students, (index, student) ->
+			$.get(window.location.origin + '/current_user', (current_student) ->
+				if current_student.id == student.id
+					if student.approved == true
+						console.log("Approved")
+						window.location.pathname = "/students/require_approval"
+					else
+						console.log("Unapproved")
+						window.location.pathname = "/students/require_approval"
+			)
+		)
+			
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -37,6 +37,7 @@ class StudentsController < ApplicationController
         flash[:error] = "There are no students to approve"
         redirect_to students_teachers_path
       else
+        WebsocketRails[:student_batch_approval].trigger 'student_batch_approval', unapproved_students
         unapproved_students.update_all(approved: true)
         flash[:success] = "You successfully approved all the students"
         redirect_to students_teachers_path(approved: true)
@@ -47,6 +48,7 @@ class StudentsController < ApplicationController
         flash[:error] = "There are no students to approve"
         redirect_to students_teachers_path(approved: true)
       else
+        WebsocketRails[:student_batch_approval].trigger 'student_batch_approval', approved_students
         approved_students.update_all(approved: false)
         flash[:success] = "You successfully unapproved all the students"
         redirect_to students_teachers_path
