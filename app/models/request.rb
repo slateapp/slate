5bad00d6d2fa0e3e567726028a87297467c772d2
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-09 09:10:25 +0100

    Extracted outside methods

--- app/models/request.rb
+++ app/models/request.rb
@@ -6,21 +6,7 @@ end
 class Request < ActiveRecord::Base
   include Statistics
   include TwilioSendMessage
-  belongs_to :student
-  belongs_to :category
-	belongs_to :teacher
-  validates :category, :description, :presence => true
-  validate :time_creation
-  scope :todays_requests, ->(minute) { where(created_at: Time.now.beginning_of_day..minute) }
-  scope :created_requests_count_for, ->(minute) { where(created_at: minute-1..minute).count }
-  scope :solved_requests_count_for, ->(minute) { where(solved_at: minute-1..minute).count }
-  scope :this_weeks_requests, -> {where(created_at: Date.today.beginning_of_week..Time.now)}
-  scope :solved_by, ->(teacher) {where(teacher: teacher)}
-  scope :categorised_by, ->(category) {where(category: category)}
-  scope :for_cohort, ->(cohort) {where(student:Student.where(cohort: cohort))}
-  scope :solved_requests, -> { where(solved: true) }
-  scope :unsolved_requests, -> { where(solved: false) }
-  after_save :trigger_teacher_message
+  include RequestScopesRelationships
 
   def time_creation
     t = Date.today
